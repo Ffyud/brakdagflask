c8b46134cb83623951f9277f6dbaba7e2c9a668f
Ffyud <5263778+Ffyud@users.noreply.github.com>
2020-12-18 18:12:05 +0100

    weghalen rommel

--- test_brakdag.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from service import BronService
-
-def testAll():
-    selectall = BronService.selectAll()
-
-    # assert selectall == 0
--- timestamp.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-from datetime import datetime
-
-today = date.today()
-midnight = datetime.combine(today, datetime.min.time())
-
-timestamp = midnight.timestamp()
-print(timestamp)
