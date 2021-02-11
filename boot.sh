49e29a2daf2f53397a36b4a18d3de6e3bda0b592
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-02-11 21:01:59 +0100

    refactor variable names

--- boot.sh
+++ boot.sh
@@ -3,4 +3,4 @@ source venv/bin/activate
 flask --version
 # flask db upgrade
 # flask translate compile
-exec gunicorn -b :5000 --access-logfile - --error-logfile - app.app:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - brakdagflask:app
