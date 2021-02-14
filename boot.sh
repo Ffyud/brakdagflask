7ade778972152aecd957166e7299960376944f39
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-02-14 14:55:52 +0100

    starten nieuws-ophalen bij starten container

--- boot.sh
+++ boot.sh
@@ -6,4 +6,4 @@ flask --version
 # flask db upgrade
 # flask translate compile
 exec gunicorn -b :5000 --access-logfile - --error-logfile - brakdagflask:app
-exec docker brakdagflask python3 nieuws-ophalen.py http://95.217.165.225:8000
+exec python3 nieuws-ophalen.py http://95.217.165.225:8000
