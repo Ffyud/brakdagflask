93374f838151bbd5f188d03547069f336ca8aae0
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-02-27 17:45:49 +0100

    niet parsen bij geen bronnen

--- nieuws-ophalen.py
+++ nieuws-ophalen.py
@@ -25,62 +25,62 @@ def nieuwsVanBronnenHalen():
         resp = requests.get(GETBRON_VAR)
         if resp.status_code != 200:
             logging.critical(resp.status_code)
-            sys.exit()
         elif resp.status_code == 200:
             logging.info('Even geduld tot alle stappen zijn doorlopen.')
             logging.info('1/6 Lijst met bronnen is opgevraagd.')
         bronnenLijst = resp.json()
         logging.info('2/6 Er zijn " + str(len(bronnenLijst))+ " bronnen gevonden.')
 
-        data=[]
-        dataAlleBronnen=[]
-        itemAttributenList=[]
-        for b in bronnenLijst:
-            bronUrl = b['link_rss']
-            bronParse = feedparser.parse(bronUrl)
-            
-            for e in bronParse['entries']:
-                # Zoveel mogelijk opschonen van description
-                opruimenHtmlTags = re.compile('<.*?>')
-                descriptionSchoonVanHtml = re.sub(opruimenHtmlTags, '', e.description)
-                descriptionSchoonEscaped = descriptionSchoonVanHtml.replace('"','\\"')
-                descriptionSchoonVanWhiteSpace = descriptionSchoonEscaped.replace("&nbsp", "").strip()
+        if(len(bronnenLijst) != 0):
+            data=[]
+            dataAlleBronnen=[]
+            itemAttributenList=[]
+            for b in bronnenLijst:
+                bronUrl = b['link_rss']
+                bronParse = feedparser.parse(bronUrl)
+                
+                for e in bronParse['entries']:
+                    # Zoveel mogelijk opschonen van description
+                    opruimenHtmlTags = re.compile('<.*?>')
+                    descriptionSchoonVanHtml = re.sub(opruimenHtmlTags, '', e.description)
+                    descriptionSchoonEscaped = descriptionSchoonVanHtml.replace('"','\\"')
+                    descriptionSchoonVanWhiteSpace = descriptionSchoonEscaped.replace("&nbsp", "").strip()
 
-                # Publicatiedatum omzetten naar een timestamp
-                publicatieDatumZonderTimeZone = ' '.join(e.published.split(' ')[:-1])
-                publicatieDatumTemplate = '%a, %d %b %Y %H:%M:%S'
-                element = datetime.datetime.strptime(publicatieDatumZonderTimeZone, publicatieDatumTemplate)
-                timestampPublicatie = int(datetime.datetime.timestamp(element)) 
+                    # Publicatiedatum omzetten naar een timestamp
+                    publicatieDatumZonderTimeZone = ' '.join(e.published.split(' ')[:-1])
+                    publicatieDatumTemplate = '%a, %d %b %Y %H:%M:%S'
+                    element = datetime.datetime.strptime(publicatieDatumZonderTimeZone, publicatieDatumTemplate)
+                    timestampPublicatie = int(datetime.datetime.timestamp(element)) 
 
-                itemAttributenList.append({"title": e.title, "link": e.link, "timestamp_publicatie": timestampPublicatie, "description": descriptionSchoonVanWhiteSpace})
+                    itemAttributenList.append({"title": e.title, "link": e.link, "timestamp_publicatie": timestampPublicatie, "description": descriptionSchoonVanWhiteSpace})
 
-        logging.info('3/6 Er zijn in totaal ' + str(len(itemAttributenList)) + ' items gevonden.')
-        foutenGevondenList = []
-        #  ombatterijen naar int ipv list
-        aantalBestaatAlInt = 0
-        aantalToegevoegdInt = 0 
-        for item in itemAttributenList:
-            if 'title' in item:
-                itemJson = json.dumps(item)
-                custom_header = {"Content-Type": "application/json"}
-                respPost = requests.post(POSTITEM_VAR, data=itemJson, headers=custom_header)
-                if respPost.status_code != 200:
-                    foutenGevondenList.append(item['title'])
-                elif respPost.status_code == 200:
-                    data = respPost.json()
-                    if 'artikel_bestaat_al' in data['resultaat']:
-                        aantalBestaatAlInt += 1
-                    elif 'goed' in data['resultaat']:    
-                        aantalToegevoegdInt += 1
+            logging.info('3/6 Er zijn in totaal ' + str(len(itemAttributenList)) + ' items gevonden.')
+            foutenGevondenList = []
+            #  ombatterijen naar int ipv list
+            aantalBestaatAlInt = 0
+            aantalToegevoegdInt = 0 
+            for item in itemAttributenList:
+                if 'title' in item:
+                    itemJson = json.dumps(item)
+                    custom_header = {"Content-Type": "application/json"}
+                    respPost = requests.post(POSTITEM_VAR, data=itemJson, headers=custom_header)
+                    if respPost.status_code != 200:
+                        foutenGevondenList.append(item['title'])
+                    elif respPost.status_code == 200:
+                        data = respPost.json()
+                        if 'artikel_bestaat_al' in data['resultaat']:
+                            aantalBestaatAlInt += 1
+                        elif 'goed' in data['resultaat']:    
+                            aantalToegevoegdInt += 1
 
-        logging.info('\n4/6 Er zijn ' + str(aantalBestaatAlInt) + ' items gevonden die al bekend zijn.')
-        logging.info('5/6 Er zijn ' + str(aantalToegevoegdInt) + ' nieuwe items gevonden.')
-        if len(foutenGevondenList) == 0:
-            logging.info('\n6/6 Nieuwsverzamelen klaar, met 0 fouten.')
-        else:
-            logging.info('Nieuwsverzamelen afgerond met ' + str(len(foutenGevondenList)) + ' fouten.')
-            for fout in foutenGevondenList:
-                logging.warning('Fout: ' + fout)
+            logging.info('\n4/6 Er zijn ' + str(aantalBestaatAlInt) + ' items gevonden die al bekend zijn.')
+            logging.info('5/6 Er zijn ' + str(aantalToegevoegdInt) + ' nieuwe items gevonden.')
+            if len(foutenGevondenList) == 0:
+                logging.info('\n6/6 Nieuwsverzamelen klaar, met 0 fouten.')
+            else:
+                logging.info('Nieuwsverzamelen afgerond met ' + str(len(foutenGevondenList)) + ' fouten.')
+                for fout in foutenGevondenList:
+                    logging.warning('Fout: ' + fout)
 
         time.sleep(300)
 
