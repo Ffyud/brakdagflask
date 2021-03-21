2aad41dd5f21da0614998c5685155adc60729f25
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-03-21 19:47:47 +0100

    apart image voor nieuws-ophaler

--- boot.sh
+++ boot.sh
@@ -6,4 +6,3 @@ flask --version
 # flask db upgrade
 # flask translate compile
 exec gunicorn -b :5000 --access-logfile - --error-logfile - brakdagflask:app
-exec python3 nieuws-ophalen.py http://95.217.165.225:8000
---
+++ newsparse/boot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source venv/bin/activate
+exec python3 nieuws-ophalen.py http://95.217.165.225:8000
