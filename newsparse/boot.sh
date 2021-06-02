fb9128870aca10945a763b685e1f766ab1806000
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-06-02 10:09:39 +0200

    test hostname

--- newsparse/boot.sh
+++ newsparse/boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source venv/bin/activate
-exec python3 nieuws-ophalen.py http://95.217.165.225:8000
-# exec python3 nieuws-ophalen.py http://brakdagflask:5000
+# exec python3 nieuws-ophalen.py http://95.217.165.225:8000
+exec python3 nieuws-ophalen.py brakdagflask:5000
