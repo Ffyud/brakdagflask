a42569f461ff0f099e8adbd19092f41daa91900f
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-06-02 10:17:14 +0200

    andere port

--- newsparse/boot.sh
+++ newsparse/boot.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-# exec python3 nieuws-ophalen.py http://95.217.165.225:8000
-exec python3 nieuws-ophalen.py http://brakdagflask:5000
+exec python3 nieuws-ophalen.py http://brakdagflask:8000
