b3364d0e938973c6a4bcee47dfe339358598c2e8
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-06-15 08:44:55 +0200

    test

--- newsparse/nieuws-ophalen.py
+++ newsparse/nieuws-ophalen.py
@@ -52,7 +52,7 @@ def nieuwsVanBronnenHalen():
     
             try:
                 bronParse = feedparser.parse(bronUrl)
-            except Exception as trace:
+            except Exception:
                 print(f'{bronUrl} kon niet bereikt worden met feedparser, maar we proberen het met een request.')
                 try:
                     request = requests.get(bronUrl)
@@ -68,7 +68,7 @@ def nieuwsVanBronnenHalen():
                         logging.critital(f'Antwoord met status {request.status_code} op {bronUrl}.')
                 except Exception as err:
                     print(f'Zowel feedparser als een request is misgegaan voor {bronUrl}.')
-                    logging.critical(f'Misgegaan met http status {request.status_code} voor {bronUrl} met: {err}.')
+                    logging.critical(f'Misgegaan voor {bronUrl} met: {err}.')
                 
             for e in bronParse['entries']:
                 # Zoveel mogelijk opschonen van description
