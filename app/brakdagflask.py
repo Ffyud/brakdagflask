56efaae695deadf2a68af94bc4c437bbd079ff16
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-06-27 16:15:39 +0200

    typo

--- app/brakdagflask.py
+++ app/brakdagflask.py
@@ -116,7 +116,7 @@ def get_item_aantal(aantal):
     cursor.execute(''' SELECT a.*, b.title as bron_title, b.logo, b.link_home
                        FROM Item as a
                        JOIN Bron as b ON a.bron_id = b.id
-                       ORDER BY a.timestamp_gevonden DESC LIMIT %d''', [count])
+                       ORDER BY a.timestamp_gevonden DESC LIMIT %s''', [count])
     mysql.connection.commit()
     rows = cursor.fetchall()
     cursor.close()
